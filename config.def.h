b033b80337a047731df907ad0aab29bb8d207358
Rod <miguelravila@outlook.com>
2020-08-10 16:46:39 -0500

    Added Nord Color scheme

--- config.def.h
+++ config.def.h
@@ -1,24 +1,23 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 600;                    /* minimum width when centered */
+static int min_width = 400;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:pixelsize=11:antialias=true:autohint=true",
-	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
+	"Proxima Nova:size=10"
 };
 static const char *prompt      = NULL;      /* -p option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#cccccc", "#282a36" },
-	[SchemeSel] = { "#ffffff", "#924441" },
-	[SchemeSelHighlight] = { "#d7d7d7", "#000000" },
-	[SchemeNormHighlight] = { "#e78481", "#000000" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-	[SchemeMid] = { "#d7d7d7", "#1f2026" },
+	/*              foreground  background */
+	[SchemeNorm] = { "#eaeaea", "#1F2229" },  /* Background color */
+	[SchemeSel] = { "#1F2229", "#88c0d0" }, /* Main selection color */
+	[SchemeSelHighlight] = { "#1F2229", "#88c0d0" }, /* Highlight slectioned color */
+	[SchemeNormHighlight] = { "#1F2229", "#a3be8c" }, /* Highlight no-slectioned color */
+	[SchemeOut] = { "#000000", "#00ffff" }, /* Secondary Color */
+	[SchemeMid] = { "#eaeaea", "#1F2229" }, /* Secondary Color */
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 22;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 18;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
@@ -28,4 +27,5 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 0;  /* -bw option; to add border width */
+
 
--- config.h
+++ config.h
@@ -3,21 +3,21 @@ static int centered = 0;                    /* -c option; centers dmenu on scree
 static int min_width = 400;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Proxima Nova:size=11"
+	"Proxima Nova:size=10"
 };
 static const char *prompt      = NULL;      /* -p option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*              foreground  background */
-	[SchemeNorm] = { "#eaeaea", "#141414" },  /* Background color */
-	[SchemeSel] = { "#eaeaea", "#3bbf7d" }, /* Main selection color */
-	[SchemeSelHighlight] = { "#141414", "#3bbf7d" }, /* Highlight slectioned color */
-	[SchemeNormHighlight] = { "#141414", "#5eade0" }, /* Highlight no-slectioned color */
+	[SchemeNorm] = { "#eaeaea", "#1F2229" },  /* Background color */
+	[SchemeSel] = { "#1F2229", "#88c0d0" }, /* Main selection color */
+	[SchemeSelHighlight] = { "#1F2229", "#88c0d0" }, /* Highlight slectioned color */
+	[SchemeNormHighlight] = { "#1F2229", "#a3be8c" }, /* Highlight no-slectioned color */
 	[SchemeOut] = { "#000000", "#00ffff" }, /* Secondary Color */
-	[SchemeMid] = { "#eaeaea", "#5eade0" }, /* Secondary Color */
+	[SchemeMid] = { "#eaeaea", "#1F2229" }, /* Secondary Color */
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 22;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 18;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
