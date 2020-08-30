6a1553a36ceeb1bce6e1eb7a5c10923b2b57b61b
Rod <miguelravila@outlook.com>
2020-08-30 10:40:35 -0500

    Added config.def

--- config.def.h
+++ config.def.h
@@ -1,6 +1,6 @@
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 400;                    /* minimum width when centered */
+static int topbar = 20;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 800;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"Proxima Nova:size=10"
@@ -8,12 +8,12 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*              foreground  background */
-	[SchemeNorm] = { "#eaeaea", "#1F2229" },  /* Background color */
-	[SchemeSel] = { "#1F2229", "#88c0d0" }, /* Main selection color */
-	[SchemeSelHighlight] = { "#1F2229", "#88c0d0" }, /* Highlight slectioned color */
-	[SchemeNormHighlight] = { "#1F2229", "#a3be8c" }, /* Highlight no-slectioned color */
+	[SchemeNorm] = { "#eaeaea", "#0f0f0f" },  /* Background color */
+	[SchemeSel] = { "#0f0f0f", "#e6d6ac" }, /* Main selection color */
+	[SchemeSelHighlight] = { "#1F2229", "#e6d6ac" }, /* Highlight slectioned color */
+	[SchemeNormHighlight] = { "#1F2229", "#e68183" }, /* Highlight no-slectioned color */
 	[SchemeOut] = { "#000000", "#00ffff" }, /* Secondary Color */
-	[SchemeMid] = { "#eaeaea", "#1F2229" }, /* Secondary Color */
+	[SchemeMid] = { "#eaeaea", "#0f0f0f" }, /* Secondary Color */
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
@@ -27,5 +27,3 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 0;  /* -bw option; to add border width */
-
-
