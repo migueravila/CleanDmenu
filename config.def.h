0b22a14019ce75ebbf4e62043fe1bf118b570af5
Miguel R. Ávika <miguelravila@outlook.com>
2020-12-10 18:50:12 -0600

    Changed def.h

--- config.def.h
+++ config.def.h
@@ -1,33 +1,24 @@
-/*  ____ _____  */
-/* |  _ \_   _|  Derek Taylor (DistroTube) */
-/* | | | || |  	http://www.youtube.com/c/DistroTube */
-/* | |_| || |  	http://www.gitlab.com/dwt1/ */
-/* |____/ |_|  	*/ 
-
-/* See LICENSE file for copyright and license details. */
-/* Default settings; can be overriden by command line. */
-
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 600;                    /* minimum width when centered */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 1105;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Hack:pixelsize=11:antialias=true:autohint=true",
+	"Proxima Nova:size=9.6",
 	"JoyPixels:pixelsize=8:antialias=true:autohint=true"
 };
 static const char *prompt      = NULL;      /* -p option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#cccccc", "#282a36" },
-	[SchemeSel] = { "#ffffff", "#924441" },
-	[SchemeSelHighlight] = { "#d7d7d7", "#000000" },
-	[SchemeNormHighlight] = { "#e78481", "#000000" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#cccccc", "#1C1B1D" },
+	[SchemeSel] = { "#1C1B1D", "#98C379" }, 
+	[SchemeSelHighlight] = { "#1C1B1D", "#98C379" },
+	[SchemeNormHighlight] = { "#cccccc", "#1C1B1D" },
+	[SchemeOut] = { "#000000", "#98C379" },
 	[SchemeMid] = { "#d7d7d7", "#1f2026" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 22;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 18;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
@@ -37,4 +28,3 @@ static const char worddelimiters[] = " ";
 
 /* Size of the window border */
 static unsigned int border_width = 0;  /* -bw option; to add border width */
-
