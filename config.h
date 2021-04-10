f27fc50345524169d3731cf14c8c80307d80355f
Miguel R. Avila <migueravila@pm.me>
2021-04-10 18:19:11 -0500

    :lipstick: feat: New background color
    Added new bg color

--- config.h
+++ config.h
@@ -5,19 +5,19 @@
 
 static int topbar = 1;
 static int centered = 1;
-static int min_width = 1150;
+static int min_width = 1100;
 static const char *fonts[] = {
 	"Inter:size=10:style=Bold",
 	"JoyPixels:pixelsize=12:antialias=true:autohint=true"};
 static const char *prompt = NULL;
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = {"#e2e2e3", "#19171A"},
-	[SchemeSel] = {"#9ed072", "#19171A"},
-	[SchemeSelHighlight] = {"#19171A", "#9ed072"},
-	[SchemeNormHighlight] = {"#e2e2e3", "#19171A"},
-	[SchemeOut] = {"#19171A", "#9ed072"},
-	[SchemeMid] = {"#e2e2e3", "#19171A"},
+	[SchemeNorm] = {"#e2e2e3", "#353237"},
+	[SchemeSel] = {"#9ed072", "#353237"},
+	[SchemeSelHighlight] = {"#353237", "#9ed072"},
+	[SchemeNormHighlight] = {"#e2e2e3", "#353237"},
+	[SchemeOut] = {"#353237", "#9ed072"},
+	[SchemeMid] = {"#e2e2e3", "#353237"},
 };
 
 static unsigned int lines = 0;
--- dmenu.c
+++ dmenu.c
@@ -18,8 +18,7 @@
 #include "drw.h"
 #include "util.h"
 
-/* macros */
-#define INTERSECT(x,y,w,h,r)  (MAX(0, MIN((x)+(w),(r).x_org+(r).width)  - MAX((x),(r).x_org)) \
+,y,w,h,r)  (MAX(0, MIN((x)+(w),(r).x_org+(r).width)  - MAX((x),(r).x_org)) \
                              * MAX(0, MIN((y)+(h),(r).y_org+(r).height) - MAX((y),(r).y_org)))
 #define LENGTH(X)             (sizeof X / sizeof X[0])
 #define TEXTW(X)              (drw_fontset_getwidth(drw, (X)) + lrpad)
